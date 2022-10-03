da3af029a83281204f5bab14bfce2d80a39e9270
binaryDiv <lexi@binarydiv.dev>
2022-10-03 18:15:41 +0200

    Add likes/dislikes to cards

--- src/objects/cards/date_cards/location_cards/LocationCard.gd
+++ src/objects/cards/date_cards/location_cards/LocationCard.gd
@@ -1,3 +1,7 @@
 tool
 class_name LocationCard
 extends BaseDateCard
+
+# Invisible card properties
+export(Array, String, "ARGame", "Cycling", "GameJam", "Gaming", "GettingASnack", "GettingCoffee", "Sightseeing", "WatchingAFilm") var good_activities := []
+export(Array, String, "ARGame", "Cycling", "GameJam", "Gaming", "GettingASnack", "GettingCoffee", "Sightseeing", "WatchingAFilm") var bad_activities := []
--- src/objects/cards/profile_cards/ProfileCard.gd
+++ src/objects/cards/profile_cards/ProfileCard.gd
@@ -8,10 +8,12 @@ onready var description_label := $DescriptionLabel as Label
 export(String, MULTILINE) var description := "Example\nText" setget set_description
 
 # Invisible card properties
-export(Array, String) var likes_profiles := []
-export(Array, String) var likes_date_ideas := []
-export(Array, String) var dislikes_profiles := []
-export(Array, String) var dislikes_date_ideas := []
+export(Array, String, "Ash", "Bastri", "Caroline", "CatrinaFish", "Chad", "Frank", "Grace", "Hannah", "Hyper", "MissNo", "Sebbi", "Waldo") var likes_profiles := []
+export(Array, String, "Ash", "Bastri", "Caroline", "CatrinaFish", "Chad", "Frank", "Grace", "Hannah", "Hyper", "MissNo", "Sebbi", "Waldo") var dislikes_profiles := []
+export(Array, String, "Beach", "Cinema", "Club", "Hackspace", "London", "Paris", "Pisa", "TheMoon") var likes_locations := []
+export(Array, String, "Beach", "Cinema", "Club", "Hackspace", "London", "Paris", "Pisa", "TheMoon") var dislikes_locations := []
+export(Array, String, "ARGame", "Cycling", "GameJam", "Gaming", "GettingASnack", "GettingCoffee", "Sightseeing", "WatchingAFilm") var likes_activities := []
+export(Array, String, "ARGame", "Cycling", "GameJam", "Gaming", "GettingASnack", "GettingCoffee", "Sightseeing", "WatchingAFilm") var dislikes_activities := []
 
 func _ready() -> void:
     self.description = description
